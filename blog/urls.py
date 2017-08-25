f5168ecb1571f251eb5a55750ee696f1b4c171b5
xs1559474822 <15683609851@163.com>
2017-08-25 20:18:02 +0800

    js

--- blog/urls.py
+++ blog/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.index),
+    url(r'^$', views.index, name='index'),
     url(r'^article/(?P<article_id>[0-9]+)/$', views.article_page, name='article_page'),
     url(r'^edt/(?P<article_id>[0-9]+)/$', views.article_change, name='edt'),
     url(r'^edt/action/$', views.edt_action, name='edt_action')
